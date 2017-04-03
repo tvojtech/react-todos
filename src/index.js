323966094e1c90ce3426a0e08594af79acf98140
Tomas Vojtech <tomas.vojtech@gmail.com>
2017-04-03 15:40:45 +0200

    setup styling using fela

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {createRenderer} from 'fela'
+import {Provider} from 'react-fela'
 
 ReactDOM.render(
-  <App />,
+  <Provider renderer={createRenderer()} mountNode={document.getElementById('felaStyles')}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import {convert} from 'css-color-function'
+
+export const colors = {
+  primary: convert('color(blue lightness(80))')
+}
